5bdfcc846c7e728d6bd0f9354155b2580e27e6b1
Santiago González <santiagogonzalez.25@campuscamara.es>
2026-06-02 09:19:43 +0200

    cambio nombres variables

--- PRESUPUESTOS_ANIO.sql
+++ PRESUPUESTOS_ANIO.sql
@@ -10,19 +10,19 @@ CREATE OR REPLACE TRIGGER empPresupuesto
     ON vistaEmpPres
     FOR EACH ROW
 DECLARE 
-    v_suma_salarios NUMBER;
-    v_presupuesto NUMBER;
+    vSumaSalarios NUMBER;
+    vPresupuesto NUMBER;
 BEGIN
-    SELECT NVL(SUM(salario * 14 + NVL(comision, 0)), 0) INTO v_suma_salarios
+    SELECT NVL(SUM(salario * 14 + NVL(comision, 0)), 0) INTO vSumaSalarios
     FROM empleados
     WHERE dept_no = :NEW.dept_no;
 
-    SELECT NVL(importe, 0) INTO v_presupuesto
+    SELECT NVL(importe, 0) INTO vPresupuesto
     FROM presupuestos
     WHERE dept_no = :NEW.dept_no
       AND anio = TO_NUMBER(TO_CHAR(SYSDATE, 'YYYY'));
 
-    IF v_suma_salarios > (v_presupuesto * 0.5) THEN 
+    IF vSumaSalarios > (vPresupuesto * 0.5) THEN 
         RAISE_APPLICATION_ERROR(-20006, 'El salario no puede exceder el 50% del presupuesto actual.');
     END IF;
 END;
@@ -33,19 +33,19 @@ CREATE OR REPLACE TRIGGER presLimite
     ON vistaEmpPres
     FOR EACH ROW
 DECLARE 
-    v_suma_salarios NUMBER;
-    v_presupuesto NUMBER;
+    vSumaSalarios NUMBER;
+    vPresupuesto NUMBER;
 BEGIN
-    SELECT NVL(SUM(salario * 14 + NVL(comision, 0)), 0) INTO v_suma_salarios
+    SELECT NVL(SUM(salario * 14 + NVL(comision, 0)), 0) INTO vSumaSalarios
     FROM empleados
     WHERE dept_no = :NEW.dept_no;
 
-    SELECT NVL(importe, 0) INTO v_presupuesto
+    SELECT NVL(importe, 0) INTO vPresupuesto
     FROM presupuestos
     WHERE dept_no = :NEW.dept_no
       AND anio = TO_NUMBER(TO_CHAR(SYSDATE, 'YYYY'));
 
-    IF v_suma_salarios > (v_presupuesto * 0.5) THEN 
+    IF vSumaSalarios > (vPresupuesto * 0.5) THEN 
         RAISE_APPLICATION_ERROR(-20007, 'El nuevo presupuesto no cubre el doble de los salarios actuales.');
     END IF;
 END;
